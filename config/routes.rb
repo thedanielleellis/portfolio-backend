c8a8814566e05264aa8918a3fd89d53cf8b31a0d
Danielle Ellis <thedanielleellis@gmail.com>
2021-07-16 14:33:40 -0500

    created namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :projects
-  resources :comments
+  namespace :api do
+    namespace :v1 do 
+      resources :projects, only: [:index, :show] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
